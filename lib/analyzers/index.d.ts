c8a0eb5ebeb3759a43b14cbad75680c3500f4994
d-s-i <briacgoujon95@gmail.com>
2022-04-29 10:01:06 +0200

    build

--- lib/analyzers/index.d.ts
+++ lib/analyzers/index.d.ts
@@ -1,2 +1,4 @@
-export {};
+export * as BlockAnalyzer from "./BlockAnalyzer";
+export * as ContractCallAnalyzer from "./ContractCallAnalyzer";
+export * as TransactionCallAnalyzer from "./TransactionCallAnalyzer";
 //# sourceMappingURL=index.d.ts.map
