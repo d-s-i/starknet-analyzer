8ccfc317c7508ad4f70e2da829725cd33ee51fef
d-s-i <briacgoujon95@gmail.com>
2022-05-06 13:46:28 +0200

    remove useless files

--- lib/analyzers/BlockAnalyzer.d.ts
+++
@@ -1,9 +0,0 @@
-import { Provider } from "starknet";
-import { GetBlockResponse } from "../types/rawStarknet";
-import { OrganizedTransaction } from "../types/organizedStarknet";
-import { TransactionCallAnalyzer } from "./TransactionCallAnalyzer";
-export declare class BlockAnalyzer extends TransactionCallAnalyzer {
-    constructor(provider: Provider);
-    organizeTransactions(block: GetBlockResponse): Promise<OrganizedTransaction[]>;
-}
-//# sourceMappingURL=BlockAnalyzer.d.ts.map
